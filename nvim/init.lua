884683f029b95887e6026c5ca1a6f14fff25c2d4
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-11-07 19:15:31 -0500

    Update with changes on laptop

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,7 +11,7 @@ vim.opt.breakindent = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = false
-
+vim.cmd('set relativenumber number')
 
 -- Leader
 vim.g.mapleader = ' '
@@ -21,7 +21,7 @@ vim.g.mapleader = ' '
 local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 local install_plugins = false
 
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then 
 	print('Installing packer...')
 	local packer_url = 'https://github.com/wbthomason/packer.nvim'
 	vim.fn.system({'git','clone','--depth','1',packer_url,install_path})
@@ -38,6 +38,17 @@ require('packer').startup(function(use)
 
 	use 'nvim-lualine/lualine.nvim'
 
+	use {'neoclide/coc.nvim', branch = 'release' }
+
+--	use {'jiangmiao/auto-pairs', requires = {'nvim-tree/nvim-web-devicons',},tag='nightly'}
+
+	use 'jiangmiao/auto-pairs'
+
+	use 'preservim/nerdtree'
+
+	use 'vim-airline/vim-airline'
+	use 'vim-airline/vim-airline-themes'
+
 	if install_plugins then
 		require('packer').sync()
 	end
@@ -48,25 +59,27 @@ if install_plugins then
 	return
 end
 
--- Restore cursor on close
+-- nvimtree
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- nerdtree configuration
+
 vim.cmd([[
-	autocmd VimLeave * jcall system('printf "\e[5 q" > $TTY')
-]])
-
--- Onedark remove background in terminals
-
-vim.cmd([[
-	if (has("autocmd") && !has("gui_running"))
-		augroup colorset
-			autocmd!
-			let s:white = {"gui": "#ABB2BF", "cterm": "145", "cterm16": "7" }
-			autocmd ColorScheme * call onedark#set_highlight("Normal",{ "fg": s:white })
-
-		augroup END
-	endif
+	" Start NERDTree when Vim starts with a directory argument.
+autocmd StdinReadPre * let s:std_in=1
+autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists('s:std_in') |
+    \ execute 'NERDTree' argv()[0] | wincmd p | enew | execute 'cd '.argv()[0] | endif
 ]])
 
 -- Other Configuration
 
 vim.opt.termguicolors = true
 vim.cmd('colorscheme onedark')
+
+vim.g.clipboard = 'unamedplus'
+vim.cmd("hi normal guibg=000000")
+vim.cmd("nmap <silent> <leader>t :NERDTreeTabsToggle<CR>")
+vim.cmd("let g:airline#extensions#tabline#enabled = 1")
+vim.cmd("set guicursor=")
