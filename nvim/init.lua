9cc1df41fe93e5de34f5d6fdd991d290ddc7a18f
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-11-08 19:25:43 -0500

    Added nerd tree to init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -21,6 +21,13 @@ cmd [[
 	set relativenumber number
 ]]
 
+-- Start NERDTree when vim starts with a directory augment
+cmd [[
+	autocmd StdinReadPre * let s:std_in=1
+	autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists('s:std_in') |
+		\ execute 'NERDTree' argv()[0] | wincmd p | enew | execute 'cd '.argv()[0] | endif
+]]
+
 
 cmd [[
 	colorscheme catppuccin
