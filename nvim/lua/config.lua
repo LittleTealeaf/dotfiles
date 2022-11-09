d96db3e0eb515e334114fb7f3307c9433243880e
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-11-08 19:33:32 -0500

    Moved configuration to config.lua

---
+++ nvim/lua/config.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+local opt = vim.opt
+local cmd = vim.cmd
+local g = vim.g
+
+function M.setup()
+  opt.smartcase = true
+  opt.ignorecase = true
+  opt.wrap = true
+  opt.mouse = 'a'
+  opt.number = true
+  opt.tabstop = 2
+  opt.shiftwidth = 2
+  opt.expandtab = false
+  opt.termguicolors = true
+  opt.cursorline = true
+
+  g.clipboard = 'unamedplus'
+
+  cmd [[
+    set relativenumber number
+  ]]
+
+  -- Start NERDTree when vim starts with a directory augment
+  cmd [[
+    autocmd StdinReadPre * let s:std_in=1
+    autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists('s:std_in') |
+      \ execute 'NERDTree' argv()[0] | wincmd p | enew | execute 'cd '.argv()[0] | endif
+  ]]
+
+
+  cmd [[
+    colorscheme catppuccin
+  ]]
+end
+
+
+return M
