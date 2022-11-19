3351a9ccd213398cb61fc0ee87b42f749bcce52b
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-11-19 16:28:27 -0500

    Added configuration for buffers

--- nvim/lua/config/plugins/tabline.lua
+++ nvim/lua/config/plugins/tabline.lua
@@ -1,11 +1,38 @@
 local tabline = require('tabline')
-
-tabline.setup({})
-
--- require('tabline').setup({})
-
 local keybind = vim.keymap.set
+local create_nvim_command = vim.api.nvim_create_user_command
 
+tabline.setup({
+	options = {
+		show_tab_always = true,
+	}
+})
 
 keybind('n','<A-[>', tabline.buffer_previous ,{} )
 keybind('n','<A-]>',tabline.buffer_next ,{})
+
+keybind('n','<A-{>', function()
+	vim.cmd('tabprev')
+end, {})
+
+keybind('n','<A-}>', function()
+	vim.cmd('tabnext')
+end, {})
+
+create_nvim_command(
+	'Tabn',
+	function(input)
+		tabline.tab_rename(input.args)
+	end,
+	{
+		nargs=1
+	}
+)
+
+create_nvim_command(
+	'ShowAllBuffers',
+	function()
+		tabline.toggle_show_all_buffers()
+	end,
+	{}
+)
