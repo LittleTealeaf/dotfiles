2e45517d848e2a248cfd69f3e1d46eb251c73197
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-11-20 15:45:21 -0500

    No warning when deleting annotated bookmarks

--- nvim/lua/config/plugins/bookmarks.lua
+++ nvim/lua/config/plugins/bookmarks.lua
@@ -2,6 +2,7 @@ vim.cmd([[
 	let g:bookmark_no_default_key_mappings = 1
 	let g:bookmark_auto_save = 1
 	let g:bookmark_save_per_working_dir = 1
+	let g:bookmark_show_warning = 0
 
 	function! g:BMWorkDirFileLocation()
 		let filename = 'vim-bookmarks'
