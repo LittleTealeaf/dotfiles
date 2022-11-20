b751b5f729ff946761a8640d09b686c731b91dae
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-11-20 13:08:40 -0500

    SNIPPETS ARE HERE

--- nvim/lua/config/plugins/coc.lua
+++ nvim/lua/config/plugins/coc.lua
@@ -27,6 +27,7 @@ g.coc_global_extensions = {
 	"coc-sh",
 	"coc-yaml",
 	"coc-prettier",
+	"coc-snippets",
 }
 
 create_command("Format", "call CocAction('format')", {})
@@ -64,11 +65,12 @@ keyset("n","<leader>cr", "<Plug>(coc-references)", {silent = true})
 
 keyset("n","<leader>ca", "<Plug>(coc-codeaction)", {silent = true})
 
--- cmd([[
---  inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm() : "\<CR>"
---  inoremap <expr> <Tab> coc#pum#visible() ? coc#pum#next(1) : "\<Tab>"
---  inoremap <expr> <S-Tab> coc#pum#visible() ? coc#pum#prev(1) : "\<S-Tab>"
+-- Snippets
 
+keyset("i","<C-l>","<Plug>(coc-snippets-expand)",{silent = true})
+keyset("i","<C-j>","<Plug>(coc-snippets-select)", {silent = true})
+
+-- Final Configuration
 cmd([[
 	let g:coc_config_home = '$DOT_FILES/nvim'
 	command! -nargs=0 Prettier :CocCommand prettier.forceFormatDocument
