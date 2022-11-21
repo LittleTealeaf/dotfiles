197b1c32cfdab9c192a3734f32c989f41245d837
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-11-21 14:43:38 -0500

    Added dotfiles and hide_tabline in dashboard

--- nvim/lua/plugins/_dashboard.lua
+++ nvim/lua/plugins/_dashboard.lua
@@ -24,7 +24,13 @@ db.custom_center = {
     icon=" ",
     desc="File Browser",
     action="Telescope file_browser"
-  }
+  },
+	{
+		icon=" ",
+		desc="Open dotfile configuration",
+		action="cd $DOT_FILES"
+	}
 }
 
- db.hide_statusline = false
+db.hide_statusline = false
+db.hide_tabline = false
