e644dca7c0ce21dba5b6bf4fe772965ce8a2820a
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-30 10:22:45 -0500

    Added keybinds for telescopes

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,7 +1,17 @@
 local telescope = require('telescope')
 local fb_actions = telescope.extensions.file_browser.actions
+local setkey = vim.keymap.set
+local actions = require('telescope.builtin')
+
 telescope.setup({
 
 })
 
 telescope.load_extension("file_browser")
+
+
+setkey('n','<leader>ff', actions.find_files, {})
+setkey('n','<leader>fg', actions.live_grep, {})
+setkey('n','<leader>fw', actions.grep_string, {})
+setkey('n','<leader>fb', actions.buffers, {})
+setkey('n','<leader>fo', actions.oldfiles, {})
