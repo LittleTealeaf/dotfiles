e7968445674b5eca62cb9da2ab78a645301231d9
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-30 10:12:12 -0500

    Finalized neotree positions

--- nvim/lua/config/neo-tree.lua
+++ nvim/lua/config/neo-tree.lua
@@ -12,11 +12,26 @@ neotree.setup({
   },
   mappings = {
     ["t"] = "",
+  },
+  event_handlers = {
+    {
+      event = "neo_tree_buffer_enter",
+      handler = function()
+        vim.cmd('highlight! Cursor blend=100')
+      end
+    },
+    {
+      event = "neo_tree_buffer_leave",
+      handler = function()
+        vim.cmd('highlight! Cursor guibg=#5f87af blend=0')
+      end
+    }
   }
 })
 
 
 
 setkey('n','<leader>tt', ':NeoTreeShowToggle<CR>', {silent=true})
-setkey('n','<leader>te', ':Neotree position=left <CR>', {silent=true})
+setkey('n','<leader>te', ':Neotree position=left filesystem <CR>', {silent=true})
 setkey('n','<leader>th', ':NeoTreeClose<CR>', {silent=true})
+setkey('n','<leader>tg', ':Neotree position=left git_status<CR>', {silent=true})
