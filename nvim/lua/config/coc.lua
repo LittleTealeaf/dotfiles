b4d583a1810ab700d06833a605ca30ef7c5c1a84
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-30 10:33:40 -0500

    added Format and Fold commands

--- nvim/lua/config/coc.lua
+++ nvim/lua/config/coc.lua
@@ -1,5 +1,7 @@
 local g = vim.g
-
+local cmd = vim.cmd
+local api = vim.api
+local create_command = api.nvim_create_user_command
 
 
 
@@ -9,3 +11,6 @@ g.coc_global_extensions = {
 }
 g.coc_config_home = "$DOT_FILES/nvim"
 
+
+create_command("Format","call CocAction('format')",{})
+create_command("Fold","call CocAction('fold', <f-args>)", {nargs = '?'})
