30a2d1c6e92f8828bc08069b2ac42f5febee74e5
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-01-09 18:54:20 -0500

    Added functions

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,31 +1,55 @@
 #!/bin/sh
 
-function cdd() {
+cdd() {
 	cd ..
 }
 
-function lless() {
+lless() {
 	ll | less
 }
 
-function rmr() {
+rmr() {
 	rm -r $1
 }
 
-function lsg() {
+lsg() {
 	ls | grep $1
 }
 
-function mkcd() {
+mkcd() {
 	mkdir $1 && cd $1
 }
 
-function vimff() {
-	vim $(find * -type f | fzf)
+vimff() {
+	local file
+	file=$(find * -type f | fzf --preview='cat {}') && vim "$file"
 }
 
-function c() {
-	cd $(find * -type d | fzf --preview='ls {}' || echo ".")
+nvimff() {
+	local file
+	file=$(find * -type f | fzf --preview='cat {}') && nvim "$file"
 }
+
+# fdf - find directory
+fdf() {
+	local dir
+	dir=$(find * -type d 2> /dev/null | fzf --preview='ls {}') && cd "$dir"
+}
+
+# fda - find all directories
+fda() {
+	local dir
+	dir=$(find ${1:-.} -type d 2> /dev/null | fzf --preview='ls {}') && cd "$dir"
+}
+
+
+# fgb - checkout git branch
+fgb() {
+	local branches branch
+	branches=$(git branch --all | grep -v HEAD) &&
+	branch=$(echo "$branches" | fzf -d $((2 + $(wc -l <<< "#branches") )) +m ) &&
+	git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+
 
 alias :Git=git
