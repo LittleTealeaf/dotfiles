e459806b866e2d155572f0fa394d4d3ce7651960
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-01-20 12:40:43 -0500

    Set max width for notify

--- nvim/lua/config/nvim-notify.lua
+++ nvim/lua/config/nvim-notify.lua
@@ -3,7 +3,9 @@ local notify = require('notify')
 notify.setup({
 	timeout = 100,
 	background_colour = "#00000000",
-	stages = "fade"
+	stages = "fade",
+	fps=60,
+	max_width = 100,
 })
 
 vim.notify = notify
