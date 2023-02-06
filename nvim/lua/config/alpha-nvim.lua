00fe4572d07175159f0dd72760c0a90f8453e08f
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-02-06 11:41:27 -0500

    changes to alpha-nvim and auto-session/neo-tree

--- nvim/lua/config/alpha-nvim.lua
+++ nvim/lua/config/alpha-nvim.lua
@@ -1,1 +1,19 @@
 local alpha = require('alpha')
+local dashboard = require('alpha.themes.dashboard')
+
+
+dashboard.section.header.val = {
+    '',
+    '  _   _        __      ___           ',
+    ' | \\ | |       \\ \\    / (_)          ',
+    ' |  \\| | ___  __\\ \\  / / _ _ __ ___  ',
+    ' | . ` |/ _ \\/ _ \\ \\/ / | | \'_ ` _ \\ ',
+    ' | |\\  |  __/ (_) \\  /  | | | | | | |',
+    ' |_| \\_|\\___|\\___/ \\/   |_|_| |_| |_|',
+    '                                     ',
+    '',
+}
+
+
+
+alpha.setup(dashboard.opts)
--- nvim/lua/config/auto-session.lua
+++ nvim/lua/config/auto-session.lua
@@ -1,7 +1,8 @@
 
-vim.cmd([[
-	let g:auto_session_pre_save_cmds = ["NeoTreeClose","AerialCloseAll"]
-]])
+-- vim.cmd([[
+-- 	let g:auto_session_pre_save_cmds = ["NeoTreeClose","AerialCloseAll"]
+-- ]])
+vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,localoptions"
 
 require("auto-session").setup({
 	log_level = "error",
@@ -12,4 +13,8 @@ require("auto-session").setup({
 		"/",
 		"~/tmp/*"
 	},
+	auto_session_pre_save_cmds = {
+		"AerialCloseAll",
+		"NeoTreeClose"
+	}
 })
