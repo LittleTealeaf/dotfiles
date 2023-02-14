cfb878b982491265a257d7ae0375316d28a1846b
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-02-14 17:34:42 -0500

    update treesitter text objects

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -4,10 +4,38 @@ require('nvim-treesitter.configs').setup({
 			enable = true,
 			lookahead = true,
 			keymaps = {
+
+				['in'] = '@number.inner',
 				['af'] = '@function.outer',
 				['if'] = '@function.inner',
+				['aF'] = '@frame.outer',
+				['iF'] = '@frame.inner',
+				['aa'] = '@assignment.outer',
+				['ia'] = '@assignment.inner',
+				['ab'] = '@block.inner',
+				['ib'] = '@block.outer',
+				['as'] = '@statement.outer',
+				['ic'] = '@class.inner',
 				['ac'] = '@class.outer',
-				['ic'] = '@class.inner'
+				['iC'] = '@call.inner',
+				['aC'] = '@call.outer',
+				['is'] = '@scopename.inner',
+
+				-- ['af'] = '@function.outer',
+				-- ['if'] = '@function.inner',
+				-- ['ac'] = '@class.outer',
+				-- ['ic'] = '@class.inner',
+				-- ['al'] = '@loop.around',
+				-- ['il'] = '@loop.inner',
+				-- ['ia'] = '@assignment.inner',
+				-- ['aa'] = '@assignment.outer',
+				-- ['ai'] = '@conditional.outer',
+				-- ['ii'] = '@conditional.inner',
+				-- ['in'] = '@number.inner',
+				-- ['ao'] = '@parameter.outer',
+				-- ['io'] = '@parameter.inner',
+				-- ['ab'] = '@block.outer',
+				-- ['ib'] = '@block.inner'
 			}
 		}
 	},
