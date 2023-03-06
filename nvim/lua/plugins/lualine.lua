a59373444445edb6ccb4a0c835c036aa621e7a27
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-03-06 12:11:43 -0500

    more refactoring

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,24 +1,23 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { 'kyazdani42/nvim-web-devicons', 'kdheepak/tabline.nvim', 'rmagatti/auto-session' },
+	name = 'lualine',
+	dependencies = {
+		{ 'kyazdani42/nvim-web-devicons' },
+		{
+			'kdheepak/tabline.nvim',
+			name = 'tabline',
+			opts = {
+				enable = false,
+				options = {
+					section_separators = { '', '' },
+					component_separators = { '', '' }
+				}
+			},
+		},
+		{ 'rmagatti/auto-session' }
+	},
 	config = function()
-		local tabline = require('tabline')
-		local lualine = require('lualine')
-		local autosession = require('auto-session-library')
-		local keybind = vim.keymap.set
-
-		keybind('n', '<A-[>', tabline.buffer_previous, {})
-		keybind('n', '<A-]>', tabline.buffer_next, {})
-
-		tabline.setup({
-			enable = false,
-			options = {
-				section_separators = { '', '' },
-				component_separators = { '', '' }
-			}
-		})
-
-		lualine.setup({
+		require('lualine').setup({
 			extensions = { 'nvim-dap-ui', 'neo-tree', 'toggleterm', 'fzf', 'fugitive', 'aerial' },
 			options = {
 				theme = 'catppuccin',
@@ -44,11 +43,16 @@ return {
 			tabline = {
 				lualine_a = {},
 				lualine_b = {},
-				lualine_c = { tabline.tabline_buffers },
-				lualine_x = { tabline.tabline_tabs },
+				lualine_c = { require('tabline').tabline_buffers },
+				lualine_x = { require('tabline').tabline_tabs },
 				lualine_y = { 'branch' },
-				lualine_z = { autosession.current_session_name }
+				lualine_z = { require('auto-session').current_session_name }
 			}
 		})
+
+		local tabline = require('tabline')
+		local keybind = vim.keymap.set
+		keybind('n', '<A-[>', tabline.buffer_previous, {})
+		keybind('n', '<A-]>', tabline.buffer_next, {})
 	end
 }
