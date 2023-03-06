55c39a52486288ac5a33e93167f451da6ba55259
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-03-06 11:59:17 -0500

    more formatting with opts instead of config

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,6 +1,6 @@
 return {
 	'folke/noice.nvim',
-	dependencies = { 'MunifTanjim/nui.nvim' },
+	dependencies = { 'MunifTanjim/nui.nvim', 'rcarriga/nvim-notify' },
 	opts = {
 		lsp = {
 			override = {
--- nvim/lua/plugins/nvim-notify.lua
+++ nvim/lua/plugins/nvim-notify.lua
@@ -1,21 +1,10 @@
 return {
 	'rcarriga/nvim-notify',
-	-- config = function()
-	-- 	local notify = require('notify')
-	--
-	-- 	notify.setup({
-	-- 		timeout = 500,
-	-- 	})
-	--
-	-- 	-- vim.notify = notify
-	-- end
-	config = function()
-		require('notify').setup({
-			timeout = 500,
-			background_colour = '#000000',
-			stages = 'fade',
-			fps = 60,
-			render = 'minimal'
-		})
-	end
+	opts = {
+		timeout = 500,
+		background_colour = '#000000',
+		stages = 'fade',
+		fps = 60,
+		render = 'minimal'
+	}
 }
