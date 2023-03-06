b0ec92f95ee3856b368d6cfbdce7c7753f20716b
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-03-06 11:57:26 -0500

    used lazy instead of custom

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,46 +1,44 @@
 return {
 	'folke/noice.nvim',
 	dependencies = { 'MunifTanjim/nui.nvim' },
-	config = function()
-		require('noice').setup({
-			lsp = {
-				override = {
-							['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-							['vim.lsp.util.stylize_markdown'] = true,
-							['cmp.entry.get_documentation'] = true
-				},
+	opts = {
+		lsp = {
+			override = {
+						['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+						['vim.lsp.util.stylize_markdown'] = true,
+						['cmp.entry.get_documentation'] = true
 			},
-			cmdline = {
-				view = 'cmdline',
-				format = {
-					cmdline = {
-						icon = ">"
-					}
+		},
+		cmdline = {
+			view = 'cmdline',
+			format = {
+				cmdline = {
+					icon = ">"
 				}
-			},
-			views = {
-				notify = {
-					replace = true,
-				},
-				messages = {
-					view = 'popup'
-				}
-			},
-			routes = {
-				{
-					filter = {
-						event = "msg_show",
-						kind = "search_count",
-					},
-					opts = {
-						skip = true
-					}
-				}
-			},
-			notify = {
-				enabled = true,
-				view = "notify"
 			}
-		})
-	end
+		},
+		views = {
+			notify = {
+				replace = true,
+			},
+			messages = {
+				view = 'popup'
+			}
+		},
+		routes = {
+			{
+				filter = {
+					event = "msg_show",
+					kind = "search_count",
+				},
+				opts = {
+					skip = true
+				}
+			}
+		},
+		notify = {
+			enabled = true,
+			view = "notify"
+		}
+	}
 }
