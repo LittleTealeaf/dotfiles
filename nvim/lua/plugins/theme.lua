1bdeb2978b9d3c0cbd4049bd4b8697dac391f05d
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-03-08 09:37:13 -0500

    formatting

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -44,10 +44,10 @@ return {
     },
     custom_highlights = function(colors)
       return {
-        -- tabline_a_normal = { fg = colors.blue, bg = 'NONE'},
-        -- tabline_b_normal = {fg = colors.text, bg = ''},
 				TelescopeBorder = {fg = colors.surface1, bg = ''},
 				CatBorder = {fg = colors.surface1, bg = ''},
+
+				-- Tabline
 				tabline_a_normal_bold_italic = {fg = colors.blue, bg = '', style = {'bold', 'italic'}},
 				tabline_b_normal_bold_italic = {fg = colors.text, bg = '', style = {'bold', 'italic'}},
 				tabline_a_normal_italic = {fg = colors.blue, bg = '', style = {'italic'}},
