ca8537d35dd7563df354e68062d519c678fccc34
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-03-08 13:27:07 -0500

    added noice to lualine

---
+++ nvim/lua/plugins/auto-session.lua
@@ -0,0 +1,12 @@
+return {
+	'rmagatti/auto-session',
+	opts = {
+		log_level = 'error',
+		auto_session_suppress_dirs = {
+			'~',
+			'~/Documents',
+			'/',
+			'~/tmp/*'
+		}
+	}
+}
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,79 +1,90 @@
 return {
-  {
-    'kdheepak/tabline.nvim',
-    dependencies = {
-      'kyazdani42/nvim-web-devicons'
-    },
-    name = 'tabline',
-    opts = {
-      enable = false,
-      options = {
-        section_separators = {'',''},
-        component_separators = { '', '' }
-      }
-    },
-    keys = {
-      {
-        '<A-[>',
-        function()
-          require('tabline').buffer_previous()
-        end
-      },
-      {
-        '<A-]>',
-        function()
-          require('tabline').buffer_next()
-        end
-      }
-    }
-  },
-  {
-    'nvim-lualine/lualine.nvim',
-    name = 'lualine',
-    dependencies = {
-      { 'kyazdani42/nvim-web-devicons' },
-      {
-        'kdheepak/tabline.nvim',
-        name = 'tabline'
-      }
-    },
-    opts = function()
-      return {
-        extensions = { 'nvim-dap-ui', 'neo-tree', 'toggleterm', 'fzf', 'fugitive', 'aerial' },
-        options = {
-          theme = 'catppuccin',
-          section_separators = {
-            left = '',
-            right = ''
-          },
-          component_separators = {
-            left = ''
-          }
-        },
-        sections = {
-          lualine_a = { 'mode' },
-          lualine_b = {},
-          lualine_c = {
-						{'filename'},
-						{'diff'}
+	{
+		'kdheepak/tabline.nvim',
+		dependencies = {
+			'kyazdani42/nvim-web-devicons'
+		},
+		name = 'tabline',
+		opts = {
+			enable = false,
+			options = {
+				section_separators = { '', '' },
+				component_separators = { '', '' }
+			}
+		},
+		keys = {
+			{
+				'<A-[>',
+				function()
+					require('tabline').buffer_previous()
+				end
+			},
+			{
+				'<A-]>',
+				function()
+					require('tabline').buffer_next()
+				end
+			}
+		}
+	},
+	{
+		'nvim-lualine/lualine.nvim',
+		name = 'lualine',
+		dependencies = {
+			{ 'folke/noice.nvim', },
+			{ 'kyazdani42/nvim-web-devicons' },
+			{
+				'kdheepak/tabline.nvim',
+				name = 'tabline'
+			}
+		},
+		opts = function()
+			local tabline = require('tabline')
+			local noice = require('noice')
+			return {
+				extensions = { 'nvim-dap-ui', 'neo-tree', 'toggleterm', 'fzf', 'fugitive', 'aerial' },
+				options = {
+					theme = 'catppuccin',
+					section_separators = {
+						left = '',
+						right = ''
 					},
-          lualine_x = {
-						{'diagnostics'},
-						{'filetype'},
+					component_separators = {
+						left = ''
+					}
+				},
+				sections = {
+					lualine_a = { 'mode' },
+					lualine_b = {},
+					lualine_c = {
+						{ 'filename' },
+						{ 'diff' }
 					},
-          lualine_y = {
+					lualine_x = {
+						{
+							noice.api.status.search.get_hl,
+							cond = noice.api.status.search.has
+						},
+						{
+							noice.api.status.command.get_hl,
+							cond = noice.api.status.command.has
+						},
+						{ 'diagnostics' },
+						{ 'filetype' },
 					},
-          lualine_z = { 'location' },
-        },
-        tabline = {
-          lualine_a = { '' },
-          lualine_b = {},
-          lualine_c = { require('tabline').tabline_buffers },
-          lualine_x = { '' },
-          lualine_y = { '' },
-          lualine_z = { '' },
-        }
-      }
-    end
-  }
+					lualine_y = {
+					},
+					lualine_z = { 'location' },
+				},
+				tabline = {
+					lualine_a = { '' },
+					lualine_b = {},
+					lualine_c = { tabline.tabline_buffers },
+					lualine_x = { '' },
+					lualine_y = { '' },
+					lualine_z = { '' },
+				}
+			}
+		end
+	}
 }
