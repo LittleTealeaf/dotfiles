3e5c9517e5970c8e78fc10bc28b370875bbbaec2
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-03-08 13:37:56 -0500

    formatted / fixed cmp

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,23 +1,28 @@
 return {
-  {
-    'windwp/nvim-autopairs',
-    dependnecies = { 'cmp' },
-    event = 'VeryLazy',
-    config = function()
-      require('nvim-autopairs').setup()
+	{
+		'windwp/nvim-autopairs',
+		dependencies = {
+			{
+				'hrsh7th/nvim-cmp',
+				name = 'cmp',
+			}
+		},
+		event = 'VeryLazy',
+		config = function()
+			require('nvim-autopairs').setup()
 
-      require('cmp').event:on(
-        'confirm_done',
-        require('nvim-autopairs.completion.cmp').on_confirm_done()
-      )
-    end
-  },
-  {
-    'windwp/nvim-ts-autotag',
-    dependnecies = { 'nvim-treesitter/nvim-treesitter' },
-    event = 'VeryLazy',
-    config = true,
-  },
+			require('cmp').event:on(
+				'confirm_done',
+				require('nvim-autopairs.completion.cmp').on_confirm_done()
+			)
+		end
+	},
+	{
+		'windwp/nvim-ts-autotag',
+		dependencies = { 'nvim-treesitter/nvim-treesitter' },
+		event = 'VeryLazy',
+		config = true,
+	},
 	{
 		'numToStr/Comment.nvim',
 		event = 'VeryLazy',
