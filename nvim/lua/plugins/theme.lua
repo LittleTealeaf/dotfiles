91ae6dafd305fb2e500e4b3e2e8ff8a31e2f9199
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-03-09 12:31:52 -0500

    added background highlight to neotree

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,50 +1,51 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  init = function()
-    -- load the colorscheme here
-    vim.cmd([[colorscheme catppuccin]])
-  end,
-  opts = {
-    flavor = "macchiato",
-    transparent_background = true,
-    integrations = {
-      neotree = true,
-      telescope = true,
-      treesitter = true,
-      treesitter_context = true,
-      noice = true,
-      notify = true,
-      markdown = true,
-      mason = true,
-      cmp = true,
-      lsp_saga = true,
-      native_lsp = {
-        enabled = true,
-        virutal_text = {
-          errors = { 'italic' },
-          hints = { 'italic' },
-          warnings = { 'italic' },
-          information = { 'italic' }
-        },
-        underlines = {
-          errors = { 'underline' },
-          hints = { 'underline' },
-          warnings = { 'underline' },
-          information = { 'underline' }
-        }
-      }
-    },
-    dim_inactive = {
-      enabled = false,
-    },
-    styles = {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	init = function()
+		-- load the colorscheme here
+		vim.cmd([[colorscheme catppuccin]])
+	end,
+	opts = {
+		flavor = "macchiato",
+		transparent_background = true,
+		integrations = {
+			neotree = true,
+			telescope = true,
+			treesitter = true,
+			treesitter_context = true,
+			noice = true,
+			notify = true,
+			markdown = true,
+			mason = true,
+			cmp = true,
+			lsp_saga = true,
+			native_lsp = {
+				enabled = true,
+				virutal_text = {
+					errors = { 'italic' },
+					hints = { 'italic' },
+					warnings = { 'italic' },
+					information = { 'italic' }
+				},
+				underlines = {
+					errors = { 'underline' },
+					hints = { 'underline' },
+					warnings = { 'underline' },
+					information = { 'underline' }
+				}
+			}
+		},
+		dim_inactive = {
+			enabled = false,
+		},
+		styles = {
 
-    },
-    custom_highlights = function(colors)
-			local border = {fg = colors.surface1, bg = ''}
-      return {
+		},
+		custom_highlights = function(colors)
+			local border = { fg = colors.surface1, bg = '' }
+			return {
+				-- Border
 				TelescopeBorder = border,
 				CatBorder = border,
 				FloatBorder = border,
@@ -54,18 +55,18 @@ return {
 				LspInfoBorder = border,
 				NeoTreeFloatBorder = border,
 				NoiceConfirmBorder = border,
-
-
+				-- Neotree
+				NeoTreeCursorLine = { bg = colors.surface1, style = { 'bold' } },
 				-- Tabline
-				tabline_a_normal_bold_italic = {fg = colors.blue, bg = '', style = {'bold', 'italic'}},
-				tabline_b_normal_bold_italic = {fg = colors.text, bg = '', style = {'bold', 'italic'}},
-				tabline_a_normal_italic = {fg = colors.blue, bg = '', style = {'italic'}},
-				tabline_b_normal_italic = {fg = colors.text, bg = '', style = {'italic'}},
-				tabline_a_normal_bold = {fg = colors.blue, bg = '', style = {'bold'}},
-				tabline_b_normal_bold = {fg = colors.text, bg = '', style = {'bold'}},
-				tabline_a_normal = {fg = colors.blue, bg = ''},
-				tabline_b_normal = {fg = colors.text, bg = ''},
-      }
-    end
-  },
+				tabline_a_normal_bold_italic = { fg = colors.blue, bg = '', style = { 'bold', 'italic' } },
+				tabline_b_normal_bold_italic = { fg = colors.text, bg = '', style = { 'bold', 'italic' } },
+				tabline_a_normal_italic = { fg = colors.blue, bg = '', style = { 'italic' } },
+				tabline_b_normal_italic = { fg = colors.text, bg = '', style = { 'italic' } },
+				tabline_a_normal_bold = { fg = colors.blue, bg = '', style = { 'bold' } },
+				tabline_b_normal_bold = { fg = colors.text, bg = '', style = { 'bold' } },
+				tabline_a_normal = { fg = colors.blue, bg = '' },
+				tabline_b_normal = { fg = colors.text, bg = '' },
+			}
+		end
+	},
 }
