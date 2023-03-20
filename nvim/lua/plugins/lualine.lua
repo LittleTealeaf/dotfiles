b4218859eaa5370a9c8c569d3842b9406184427f
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-03-20 13:02:40 -0400

    separated tabline into its own plugin directory

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,25 +1,5 @@
 return {
 	{
-		'kdheepak/tabline.nvim',
-		dependencies = {
-			'kyazdani42/nvim-web-devicons'
-		},
-		name = 'tabline',
-		opts = {
-			enable = false,
-			options = {
-				section_separators = { '', '' },
-				component_separators = { '', '' }
-			}
-		},
-		keys = {
-			{ '<A-[>',      ':TablineBufferPrevious<CR>', desc = "Goto Prev Buffer" },
-			{ '<A-]>',      ':TablineBufferNext<CR>',     desc = 'Goto Next Buffer' },
-			{ '<leader>bd', ':bd<CR>',                    desc = "Delete Buffer" },
-			{ '<leader>bc', ':%bd<CR>',                   desc = "Delete All Buffers" }
-		}
-	},
-	{
 		'nvim-lualine/lualine.nvim',
 		name = 'lualine',
 		dependencies = {
@@ -49,8 +29,8 @@ return {
 					lualine_a = { 'mode' },
 					lualine_b = {},
 					lualine_c = {
-						{ 'filename' },
-						{ 'filetype' },
+						{ 'filetype', icon_only = true},
+						{ 'filename', path = 1 },
 						{ 'diff' },
 					},
 					lualine_x = {
---
+++ nvim/lua/plugins/tabline.lua
@@ -0,0 +1,30 @@
+local tabline_command = function(command)
+	return function()
+		require('tabline')[command]()
+	end
+end
+
+
+return {
+	{
+		'kdheepak/tabline.nvim',
+		dependencies = {
+			'kyazdani42/nvim-web-devicons'
+		},
+		name = 'tabline',
+		opts = {
+			enable = false,
+			options = {
+				section_separators = { '', '' },
+				component_separators = { '', '' },
+				show_filename_only = true,
+			}
+		},
+		keys = {
+			{ '<A-[>',      tabline_command('buffer_next'),     desc = "Goto Prev Buffer" },
+			{ '<A-]>',      tabline_command('buffer_previous'), desc = 'Goto Next Buffer' },
+			{ '<leader>bd', ':bd<CR>',                          desc = "Delete Buffer" },
+			{ '<leader>bc', ':%bd<CR>',                         desc = "Delete All Buffers" }
+		}
+	}
+}
