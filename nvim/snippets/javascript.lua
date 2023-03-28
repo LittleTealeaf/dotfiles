de7fa78dc739a35e1c03933ed0b6186fdbe746fb
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-03-28 10:33:26 -0400

    continued working on snippets

--- nvim/snippets/javascript.lua
+++ nvim/snippets/javascript.lua
@@ -1,5 +1,6 @@
 return {
-	s("log", sn(1,{
+	-- Console Log
+	s("log", sn(1, {
 		t('console.log('),
 		i(1, ''),
 		t(')')
--- nvim/snippets/python.lua
+++ nvim/snippets/python.lua
@@ -1,5 +1,15 @@
 return {
+	-- Imports
 	s("impnp", t("import numpy as np")),
 	s("imppd", t("import pandas as pd")),
-	s("imptf", t("import tensorflow as tf"))
+	s("imptf", t("import tensorflow as tf")),
+	-- Printing
+	s("pr", sn(1, { t("print("), i(1), t(')') })),
+	s("prs", sn(1, { t("print('"), i(1), t("')") })),
+	s("prf", sn(1, { t("print(f'"), i(1), t("')") })),
+	-- Main
+	s("main", {
+		t({ 'if __name__ == "__main__":', '' }),
+		t('\t'), i(1)
+	})
 }, {}
