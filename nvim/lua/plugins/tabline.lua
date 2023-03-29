4097a1f2ac619424b18f35ca1c570180b9a46476
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-03-29 08:50:34 -0400

    formatting

--- nvim/lua/plugins/tabline.lua
+++ nvim/lua/plugins/tabline.lua
@@ -23,8 +23,8 @@ return {
 		keys = {
 			{ '<A-[>',      tabline_command('buffer_previous'), desc = "Goto Prev Buffer" },
 			{ '<A-]>',      tabline_command('buffer_next'),     desc = 'Goto Next Buffer' },
-			{ '<leader>bd', '<cmd>bd<CR>',                          desc = "Delete Buffer" },
-			{ '<leader>bc', '<cmd>%bd<CR>',                         desc = "Delete All Buffers" }
+			{ '<leader>bd', '<cmd>bd<CR>',                      desc = "Delete Buffer" },
+			{ '<leader>bc', '<cmd>%bd<CR>',                     desc = "Delete All Buffers" }
 		}
 	}
 }
