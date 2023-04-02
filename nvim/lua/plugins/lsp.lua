67364309f747eed1b92dfd030e02b0b1baa16144
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-04-02 08:41:00 -0400

    fixed keymapping for nvim-navbuddy

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,4 +1,3 @@
-
 return {
 	{
 		'lvimuser/lsp-inlayhints.nvim',
@@ -15,43 +14,67 @@ return {
 			{ "SmiteshP/nvim-navic", opts = { highlight = true } },
 			"MunifTanjim/nui.nvim"
 		},
-		opts = {
-			window = {
-				border = "rounded",
-				size = "75%",
-				sections = {
-					left = {
-						size = "25%"
-					},
-					mid = {
-						size = "50%"
+		opts = function()
+			local actions = require('nvim-navbuddy.actions')
+			return {
+				window = {
+					border = "rounded",
+					size = "75%",
+					sections = {
+						left = {
+							size = "25%"
+						},
+						mid = {
+							size = "50%"
+						}
 					}
-				}
-			},
-			lsp = {
-				auto_attach = true
-			},
-			icons = {
-				Boolean = "󰔡 "
-			},
-			mappings = {
-				["l"] = function(display)
-					if display.focus_node.children == nil then
-						return
-					end
+				},
+				lsp = {
+					auto_attach = true
+				},
+				icons = {
+					Boolean = "󰔡 "
+				},
+				mappings = {
+					["l"] = function(display)
+						if display.focus_node.children == nil then
+							return
+						end
 
-					local child_node
-					if display.focus_node.memory then
-						child_node = display.focus_node.children[display.focus_node.memory]
-					else
-						child_node = display.focus_node.children[1]
-					end
-					display.focus_node = child_node
+						local child_node
+						if display.focus_node.memory then
+							child_node = display.focus_node.children[display.focus_node.memory]
+						else
+							child_node = display.focus_node.children[1]
+						end
+						display.focus_node = child_node
 
-					display:redraw()
-				end
+						display:redraw()
+					end,
+					['<esc>'] = actions.close,
+					['q'] = actions.close,
+					['j'] = actions.next_sibling,
+					['k'] = actions.previous_sibling,
+					['h'] = actions.parent,
+					['v'] = actions.visual_name,
+					['V'] = actions.visual_scope,
+					['y'] = actions.yank_name,
+					['Y'] = actions.yank_scope,
+					['i'] = actions.insert_name,
+					['I'] = actions.insert_scope,
+					['a'] = actions.append_name,
+					['A'] = actions.append_scope,
+					['r'] = actions.rename,
+					['d'] = actions.delete,
+					['f'] = actions.fold_create,
+					['F'] = actions.fold_delete,
+					['<enter>'] = actions.select,
+					['o'] = actions.select,
+					['J'] = actions.move_up,
+					['K'] = actions.move_down,
+				},
 			}
-		},
+		end,
 		keys = {
 			{ '<leader>cm', function() require('nvim-navbuddy').open() end, desc = "Open Navbuddy" }
 		}
