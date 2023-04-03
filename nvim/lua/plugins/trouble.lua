d2ac8c2046bfcf59336abc303eab800d49ed4b90
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-04-03 12:22:43 -0400

    use trouble instead of glance for references

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -16,6 +16,6 @@ return {
 	},
 	keys = {
 		{ '<leader>ce', '<cmd>Trouble workspace_diagnostics<CR>', desc = "Toggle Trouble" },
-		-- { '<leader>cr', '<cmd>Trouble lsp_references<CR>',        desc = "LSP references" }
+		{ '<leader>cr', '<cmd>Trouble lsp_references<CR>',        desc = "LSP references" }
 	}
 }
