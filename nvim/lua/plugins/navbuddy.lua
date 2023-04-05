225ed8616522766e2dd93e1439d37d176fc6040f
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-04-05 15:15:22 -0400

    no more need for a "function" for opts

--- nvim/lua/plugins/navbuddy.lua
+++ nvim/lua/plugins/navbuddy.lua
@@ -11,46 +11,43 @@ return {
 			"SmiteshP/nvim-navic",
 			"MunifTanjim/nui.nvim"
 		},
-		opts = function()
-			local actions = require('nvim-navbuddy.actions')
-			return {
-				window = {
-					border = "rounded",
-					size = "75%",
-					sections = {
-						left = {
-							size = "25%"
-						},
-						mid = {
-							size = "50%"
-						}
+		opts = {
+			window = {
+				border = "rounded",
+				size = "75%",
+				sections = {
+					left = {
+						size = "25%"
+					},
+					mid = {
+						size = "50%"
 					}
-				},
-				lsp = {
-					auto_attach = true
-				},
-				icons = {
-					Boolean = "󰔡 "
-				},
-				mappings = {
-					["l"] = function(display)
-						if display.focus_node.children == nil then
-							return
-						end
+				}
+			},
+			lsp = {
+				auto_attach = true
+			},
+			icons = {
+				Boolean = "󰔡 "
+			},
+			mappings = {
+				["l"] = function(display)
+					if display.focus_node.children == nil then
+						return
+					end
 
-						local child_node
-						if display.focus_node.memory then
-							child_node = display.focus_node.children[display.focus_node.memory]
-						else
-							child_node = display.focus_node.children[1]
-						end
-						display.focus_node = child_node
+					local child_node
+					if display.focus_node.memory then
+						child_node = display.focus_node.children[display.focus_node.memory]
+					else
+						child_node = display.focus_node.children[1]
+					end
+					display.focus_node = child_node
 
-						display:redraw()
-					end,
-				},
-			}
-		end,
+					display:redraw()
+				end,
+			},
+		},
 		keys = {
 			{ '<leader>cm', function() require('nvim-navbuddy').open() end, desc = "Open Navbuddy" }
 		}
