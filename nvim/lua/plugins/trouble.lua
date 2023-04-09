07498a2f3d3379cc9037ffd0185096fd004a6967
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-04-09 12:30:21 -0400

    organized / updated trouble open keybinds

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -7,22 +7,24 @@ return {
 		auto_open = false,
 		height = 15,
 		auto_close = true,
+		auto_fold = true,
 		action_keys = {
 			jump_close = '<CR>',
 			jump = nil,
 			open_folds = 'H',
 			close_folds = 'h',
-			toggle_fold = 'l',
+			toggle_fold = 'l'
 		},
 		use_diagnostic_signs = true,
 	},
 	keys = {
-		{ '<leader>te', '<cmd>Trouble workspace_diagnostics<CR>', desc = "Toggle Trouble" },
-		{ '<leader>tr', '<cmd>Trouble lsp_references<CR>',        desc = "LSP references" },
-		{ '<leader>td', '<cmd>Trouble lsp_definitions<CR>',       desc = 'LSP Definitions' },
-		{ '<leader>ti', '<cmd>Trouble lsp_implementations<CR>',   desc = "LSP Implementations" },
-		{ '<leader>ty', '<cmd>Trouble lsp_type_definitions<CR>',  desc = "LSP Type Definitions" },
+		{ '<leader>te', '<cmd>Trouble workspace_diagnostics<CR>', desc = "Open Trouble Diagnostics" },
+		{ '<leader>tr', '<cmd>Trouble lsp_references<CR>',        desc = "Open Trouble Lsp References" },
+		{ '<leader>td', '<cmd>Trouble lsp_definitions<CR>',       desc = "Open Trouble Lsp Definitions"},
+		{ '<leader>ti', '<cmd>Trouble lsp_implementations<CR>',   desc = "Open Trouble Lsp Implementations" },
+		{ '<leader>ty', '<cmd>Trouble lsp_type_definitions<CR>',  desc = "Open Trouble Lsp Type Definitions" },
+		{ '<leader>tf', '<cmd>Trouble telescope<CR>',             desc = "Open Trouble Telescope" },
 		{ '<leader>tt', '<cmd>Trouble<CR>',                       desc = "Toggle Trouble" },
-		{ '<leader>th', '<cmd>TroubleClose<CR>',                  desc = "Close Trouble Window" }
+		{ '<leader>th', '<cmd>TroubleClose<CR>',                  desc = "Close Trouble Window" },
 	}
 }
