e6f29f29151086b3002d4aa0d9fd3754f6b47fc3
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-04-14 13:49:29 -0400

    remove all lualine when inactive

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -63,6 +63,14 @@ return {
 						}
 					},
 				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {},
+				}
 			}
 		end
 	}
