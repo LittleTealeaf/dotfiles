e3c6f0189093d45c665b51e2cf61d042512137eb
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-04-19 18:12:55 -0400

    telescope uses ivy for navbuddy

--- nvim/lua/plugins/navbuddy.lua
+++ nvim/lua/plugins/navbuddy.lua
@@ -13,45 +13,51 @@ return {
 			{
 				'nvim-telescope/telescope.nvim',
 				name = 'telescope'
-			}
+			},
+			"numToStr/Comment.nvim",
 		},
-		opts = {
-			window = {
-				border = "rounded",
-				size = "75%",
-				sections = {
-					left = {
-						size = "25%"
-					},
-					mid = {
-						size = "50%"
+		opts = function()
+			local actions = require('nvim-navbuddy.actions')
+			local ivy = require("telescope.themes").get_ivy({})
+			return {
+				window = {
+					border = "rounded",
+					size = "75%",
+					sections = {
+						left = {
+							size = "20%"
+						},
+						mid = {
+							size = "30%"
+						}
 					}
-				}
-			},
-			lsp = {
-				auto_attach = true
-			},
-			icons = {
-				Boolean = "󰔡 "
-			},
-			mappings = {
-				["l"] = function(display)
-					if display.focus_node.children == nil then
-						return
-					end
+				},
+				lsp = {
+					auto_attach = true
+				},
+				icons = {
+					Boolean = "󰔡 "
+				},
+				mappings = {
+					["l"] = function(display)
+						if display.focus_node.children == nil then
+							return
+						end
 
-					local child_node
-					if display.focus_node.memory then
-						child_node = display.focus_node.children[display.focus_node.memory]
-					else
-						child_node = display.focus_node.children[1]
-					end
-					display.focus_node = child_node
+						local child_node
+						if display.focus_node.memory then
+							child_node = display.focus_node.children[display.focus_node.memory]
+						else
+							child_node = display.focus_node.children[1]
+						end
+						display.focus_node = child_node
 
-					display:redraw()
-				end,
-			},
-		},
+						display:redraw()
+					end,
+					["t"] = actions.telescope(ivy)
+				},
+			}
+		end,
 		keys = {
 			{ '<leader>cm', function() require('nvim-navbuddy').open() end, desc = "Open Navbuddy" }
 		}
