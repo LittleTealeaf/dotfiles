30d8e24b19a609570392b18913a81d2ca9bb5da3
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-05-09 18:36:49 -0400

    fixed navbuddy

--- nvim/lua/plugins/navbuddy.lua
+++ nvim/lua/plugins/navbuddy.lua
@@ -39,21 +39,23 @@ return {
 					Boolean = "󰔡 "
 				},
 				mappings = {
-					["l"] = function(display)
-						if display.focus_node.children == nil then
-							return
-						end
+					["l"] = {
+						callback = function(display)
+							if display.focus_node.children == nil then
+								return
+							end
 
-						local child_node
-						if display.focus_node.memory then
-							child_node = display.focus_node.children[display.focus_node.memory]
-						else
-							child_node = display.focus_node.children[1]
-						end
-						display.focus_node = child_node
+							local child_node
+							if display.focus_node.memory then
+								child_node = display.focus_node.children[display.focus_node.memory]
+							else
+								child_node = display.focus_node.children[1]
+							end
+							display.focus_node = child_node
 
-						display:redraw()
-					end,
+							display:redraw()
+						end
+					},
 					["t"] = actions.telescope(ivy)
 				},
 			}
