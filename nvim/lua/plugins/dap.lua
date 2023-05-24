82681681c822b3baca54872bda0c5e2e2a69a2c6
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-05-24 13:07:35 -0400

    changes

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -57,6 +57,44 @@ return {
 			require('dapui').setup({
 				floating = {
 					border = 'rounded'
+				},
+				layouts = {
+					{
+						elements = {
+							{
+								id = 'scopes',
+								size = 0.25
+							},
+							{
+								id = 'breakpoints',
+								size = 0.25,
+							},
+							{
+								id = 'stacks',
+								size = 0.25
+							},
+							{
+								id = 'watches',
+								size = 0.25,
+							}
+						},
+						position = 'right',
+						size = 50
+					},
+					{
+						elements = {
+							{
+								id = 'repl',
+								size = 0.5
+							},
+							{
+								id = 'console',
+								size = 0.5
+							},
+						},
+						position = 'bottom',
+						size = 10
+					}
 				}
 			})
 
