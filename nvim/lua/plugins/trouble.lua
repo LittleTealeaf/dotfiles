c981a3728b7d2d9192f6ed51fb0eb72d06d12767
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-05-27 13:52:31 -0400

    practicing substitution

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -8,7 +8,7 @@ return {
 		height = 15,
 		auto_close = true,
 		action_keys = {
-			jump_close = '<CR>',
+			jump_close = '<cr>',
 			jump = nil,
 			open_folds = 'H',
 			close_folds = 'h',
@@ -17,14 +17,14 @@ return {
 		use_diagnostic_signs = true,
 	},
 	keys = {
-		{ '<leader>te', '<cmd>Trouble workspace_diagnostics<CR>', desc = "Open Trouble Diagnostics" },
-		{ '<leader>tr', '<cmd>Trouble lsp_references<CR>',        desc = "Open Trouble Lsp References" },
-		{ '<leader>td', '<cmd>Trouble lsp_definitions<CR>',       desc = "Open Trouble Lsp Definitions" },
-		{ '<leader>ti', '<cmd>Trouble lsp_implementations<CR>',   desc = "Open Trouble Lsp Implementations" },
-		{ '<leader>ty', '<cmd>Trouble lsp_type_definitions<CR>',  desc = "Open Trouble Lsp Type Definitions" },
-		{ '<leader>tq', '<cmd>Trouble quickfix<CR>',              desc = "Open Quickfix in Trouble" },
-		{ '<leader>tf', '<cmd>Trouble telescope<CR>',             desc = "Open Trouble Telescope" },
-		{ '<leader>tt', '<cmd>Trouble<CR>',                       desc = "Toggle Trouble" },
-		{ '<leader>th', '<cmd>TroubleClose<CR>',                  desc = "Close Trouble Window" },
+		{ '<leader>te', '<cmd>Trouble workspace_diagnostics<cr>', desc = "Open Trouble Diagnostics" },
+		{ '<leader>tr', '<cmd>Trouble lsp_references<cr>',        desc = "Open Trouble Lsp References" },
+		{ '<leader>td', '<cmd>Trouble lsp_definitions<cr>',       desc = "Open Trouble Lsp Definitions" },
+		{ '<leader>ti', '<cmd>Trouble lsp_implementations<cr>',   desc = "Open Trouble Lsp Implementations" },
+		{ '<leader>ty', '<cmd>Trouble lsp_type_definitions<cr>',  desc = "Open Trouble Lsp Type Definitions" },
+		{ '<leader>tq', '<cmd>Trouble quickfix<cr>',              desc = "Open Quickfix in Trouble" },
+		{ '<leader>tf', '<cmd>Trouble telescope<cr>',             desc = "Open Trouble Telescope" },
+		{ '<leader>tt', '<cmd>Trouble<cr>',                       desc = "Toggle Trouble" },
+		{ '<leader>th', '<cmd>TroubleClose<cr>',                  desc = "Close Trouble Window" },
 	}
 }
