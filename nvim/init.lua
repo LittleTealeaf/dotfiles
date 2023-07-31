b92e81c580106c81c6b3af2abadb4710286b944e
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-07-31 07:09:25 -0400

    Added vscode switch

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,14 @@
-if vim.g.neovide then
-	require("config.neovide")
+if vim.g.vscode then
+
+else
+	if vim.g.neovide then
+		require("config.neovide")
+	end
+	require('config.options')
+	require('config.commands')
+	require('config.keybinds')
+	require('config.lazy')
 end
-require('config.options')
-require('config.commands')
-require('config.keybinds')
-require('config.lazy')
 
 
 -- TODO: try out bufferline again?
