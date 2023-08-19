a9f610ea0d94da0db5c579a66701a20991bb0815
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-08-19 10:02:30 -0400

    separate fzf config

---
+++ bash/fzf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export FZF_DEFAULT_OPTS='--layout=reverse --info=inline --color=fg:#cad3f5,bg:-1,hl:#ee99a0 --color=fg+:#b7bdf8,bg+:,hl+:#ed8796 --color=info:#b7bdf8,prompt:#b7bdf8,pointer:#b7bdf8 --color=marker:#b7bdf8,spinner:#b7bdf8,header:#b7bdf8,border:#b7bdf8,bg+:#24273a'
+
+
