d4f91d667d2a0fd8f718de50600999e40575d9ac
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-08-19 09:48:41 -0400

    customized dev project preview

--- bash/functions.sh
+++ bash/functions.sh
@@ -4,7 +4,6 @@
 # Especially for functions that use fzf to extend a function, it will be in the notation of "base cmd" + "operation"
 # For example, to open a git project with nvim, the command would be nvimg, similarly to cd to a git project woudl be cdg
 
-
 # shortcut for cd ..
 cdd() {
 	cd ..
@@ -14,17 +13,10 @@ cddev() {
 	cd ~/dev
 }
 
-# Opens a fzf instance to select projects within the ~/git directory, and then return the full path
-# The title can be specified in the first parameter
 fzf_git_project() {
 	local selection
 	selection=$(ls ~/git | fzf  --preview='ls -l ~/git/{}' --header-first --header="$1") &&
 		echo ~/git/$selection
-}
-
-fzf_dev() {
-	local selection
-	selection=$(ls ~/dev | fzf --preview='ls -l ~/dev/{}' --header-first --header="$1") && echo ~/dev/$selection
 }
 
 cdg() {
@@ -33,21 +25,12 @@ cdg() {
 }
 
 cdv() {
-	local dir
-	dir=$(fzf_dev "Open Development Project") && cd "$dir"
-}
-
-nvimg() {
-	local dir
-	dir=$(fzf_git_project "open git project in neovim") && cd "$dir" && nvim
+	local selection
+	selection=$(ls ~/dev | fzf --preview='project_info ~/dev/{}' --header-first --header="Open Development Project") && cd ~/dev && cd $selection
 }
 
 cddot() {
 	cd $DOT_FILES
-}
-
-nvimdot() {
-	cddot && nvim
 }
 
 fzf_directory() {
@@ -64,11 +47,9 @@ nvimf() {
 	dir=$(fzf_directory "open directory in neovim") && nvim $dir
 }
 
-
 fzf_file() {
 	find . -type f | fzf --preview='cat {}' --header-first --header="$1"
 }
-
 
 cdgr() {
 	cd $(git rev-parse --show-toplevel)
