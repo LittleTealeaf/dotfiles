8a403b3fc0a0c8dc2f3a238bab72beaead4c1764
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-08-21 15:23:57 -0400

    changes

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,3 +1,9 @@
+function nav_file(index)
+	return function ()
+		require('harpoon.ui').nav_file(index)
+	end
+end
+
 return {
 	'ThePrimeagen/harpoon',
 	dependnecies = {
@@ -13,5 +19,15 @@ return {
 		{ '<leader>bg', function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle Harpoon Quick Menu" },
 		{ '<A-S-]>',      function() require('harpoon.ui').nav_next() end,          desc = "Next Harpoon File" },
 		{ '<A-S-[>',     function() require('harpoon.ui').nav_prev() end,          desc = "Next Harpoon File" },
+		{'<A-1>', nav_file(1), desc = "Harpooned File 1"},
+		{'<A-2>', nav_file(2), desc = "Harpooned File 2"},
+		{'<A-3>', nav_file(3), desc = "Harpooned File 3"},
+		{'<A-4>', nav_file(4), desc = "Harpooned File 4"},
+		{'<A-5>', nav_file(5), desc = "Harpooned File 5"},
+		{'<A-6>', nav_file(6), desc = "Harpooned File 6"},
+		{'<A-7>', nav_file(7), desc = "Harpooned File 7"},
+		{'<A-8>', nav_file(8), desc = "Harpooned File 8"},
+		{'<A-9>', nav_file(9), desc = "Harpooned File 9"},
+		{'<A-0>', nav_file(10), desc = "Harpooned File 10"},
 	}
 }
