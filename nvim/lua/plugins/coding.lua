0c09a3038b05b971d87c0d735a131d6301c45a89
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-09-28 13:02:36 -0400

    remove repl

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -25,22 +25,5 @@ return {
 				extra = true
 			}
 		}
-	},
-	{
-		'pappasam/nvim-repl',
-		init = function()
-			vim.g.repl_split = 'bottom'
-			vim.g.repl_height = 20
-			vim.g.repl_filetype_commands = {
-				python = 'ipython --no-autoindent'
-			}
-		end,
-		keys = {
-			{ '<leader>ro', '<cmd>ReplOpen<CR>',    desc = "Open REPL" },
-			{ '<leader>rs', '<cmd>ReplRunCell<CR>', desc = "REPL Run Cell" },
-			{ '<leader>rs', '<cmd>ReplSend<CR>',    desc = 'REPL Run Selected', mode = { 'v' } },
-			{ '<leader>rh', '<cmd>ReplClose<CR>',   desc = 'Close REPL' },
-			{ '<leader>rc', '<cmd>ReplClear<CR>',   desc = 'Clear REPp' }
-		}
 	}
 }
