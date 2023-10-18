d81f51ea04245cfb05cbf3037891b104e3dda68a
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-10-18 11:51:10 -0400

    added variable font size for neovide

--- nvim/lua/config/neovide.lua
+++ nvim/lua/config/neovide.lua
@@ -1,3 +1,28 @@
 vim.g.neovide_refresh_rate = 60
 vim.g.neovide_transparency = 0.5
-vim.o.guifont = "FiraCode Nerd Font"
+
+
+
+NEOVIDE = {
+	font = {
+		size = 14
+	}
+}
+
+
+local function updateNeovideFont()
+	vim.o.guifont = "FiraCode Nerd Font:h" .. NEOVIDE.font.size
+end
+
+
+updateNeovideFont()
+
+vim.keymap.set("n", "<C-+>", function()
+	NEOVIDE.font.size = NEOVIDE.font.size + 1
+	updateNeovideFont()
+end, { desc = "Increase Font Size" })
+
+vim.keymap.set("n", "<C-_>", function()
+	NEOVIDE.font.size = NEOVIDE.font.size - 1
+	updateNeovideFont()
+end, { desc = "Decrease Font Size" })
