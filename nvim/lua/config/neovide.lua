5837d5ad806a587f8a52036e8b2c6b1076294605
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-10-19 13:22:39 -0400

    add sudo config

--- nvim/lua/config/neovide.lua
+++ nvim/lua/config/neovide.lua
@@ -1,5 +1,5 @@
 vim.g.neovide_refresh_rate = 60
-vim.g.neovide_transparency = 0.5
+vim.g.neovide_transparency = 1
 
 vim.o.guifont = "FiraCode Nerd Font:h14"
 
@@ -13,3 +13,4 @@ local scale_delta = 1.1
 
 vim.keymap.set('n', '<C-+>', function() change_scale_factor(scale_delta) end)
 vim.keymap.set('n', '<C-_>', function() change_scale_factor(1 / scale_delta) end)
+vim.keymap.set('n','<C-S-BS>', function() vim.g.neovide_scale_factor = 1 end)
