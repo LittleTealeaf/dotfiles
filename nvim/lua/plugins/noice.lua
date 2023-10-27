a7f8087ba9fdd1f07ae0c5558e1395b7a7920a47
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-10-27 12:29:54 -0400

    changes

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -15,9 +15,6 @@ return {
 				}
 			},
 			lsp = {
-				progress = {
-					view = 'mini',
-				},
 				override = {
 					['vim.lsp.util.convert_input_to_markdown_lines'] = true,
 					['vim.lsp.util.stylize_markdown'] = true,
@@ -29,7 +26,7 @@ return {
 				},
 				hover = {
 					silent = true,
-				}
+				},
 			},
 			presets = {
 				lsp_doc_border = Transparent,
