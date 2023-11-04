86e2a9ce4e30c8d58b5f3b3913abaebd8ef259ce
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-11-04 13:35:33 -0400

    remove markdown preview

--- nvim/lua/plugins/markdown-preview.lua
+++
@@ -1,9 +0,0 @@
-return {
-	'iamcco/markdown-preview.nvim',
-	build = 'cd app && npm install --no-save',
-	ft = { 'markdown', 'md' },
-	init = function()
-		vim.g.mkdp_theme = 'dark'
-	end,
-	enabled = false
-}
