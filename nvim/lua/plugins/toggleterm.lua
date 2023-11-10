ea104774c0265430cfebc0554b55a10d797701e9
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-11-10 14:36:45 -0500

    changes

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,8 +1,19 @@
 return {
 	'akinsho/toggleterm.nvim',
+	init = function()
+		vim.api.nvim_create_autocmd('TermOpen', {
+			desc = 'Set Terminal Keymaps',
+			callback = function()
+				local opts = { buffer = 0 }
+				vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+			end,
+			pattern = 'term://*toggleterm#*'
+		})
+	end,
 	opts = {
 		open_mapping = [[<c-\>]],
-		direction = 'float',
+		size = 20,
+		direction = 'horizontal',
 		float_opts = {
 			border = 'curved',
 		},
