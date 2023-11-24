1ca96fec18f779a8ce75d44257bf9b7c22afbc16
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-11-24 11:25:59 -0500

    changes

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -3,9 +3,12 @@ return {
 	dependencies = { 'nvimtree/nvim-web-devicons' },
 	opts = {
 		keymaps = {
+			['<C-v>'] = 'actions.select_vsplit',
+			['<C-x>'] = 'actions.select_split',
 			['<M-q>'] = 'actions.close'
 		}
 	},
+	lazy = false,
 	config = true,
 	keys = {
 		{ '<leader>ef', function() require('oil').open() end, desc = "Open Oil" },
