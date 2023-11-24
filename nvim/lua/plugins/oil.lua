7de161a336e4339ea119c98dbd40be9d12b6a86a
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-11-24 16:56:12 -0500

    changes

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -5,14 +5,15 @@ return {
 		keymaps = {
 			['<C-v>'] = 'actions.select_vsplit',
 			['<C-x>'] = 'actions.select_split',
-			['<M-q>'] = 'actions.close'
+			['<M-q>'] = 'actions.close',
+			['<C-t>'] = false,
+			['<C-y>'] = 'actions.copy_entry_path'
 		}
 	},
 	lazy = false,
 	config = true,
 	keys = {
-		{ '<leader>ef', function() require('oil').open() end, desc = "Open Oil" },
+		{ '<leader>ef', function() require('oil').open() end,       desc = "Open Oil" },
 		{ '<leader>eg', function() require('oil').open_float() end, desc = "Open Oil Float" },
-		-- { '<leader>eb', function() require('oil').open() end, desc = "Open Oil" }
 	}
 }
