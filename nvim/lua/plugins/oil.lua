52d68298c0d9072bcdd64412d7da6bade06cb502
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-11-25 10:37:17 -0500

    oil rename feature?

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -7,7 +7,26 @@ return {
 			['<C-x>'] = 'actions.select_split',
 			['<M-q>'] = 'actions.close',
 			['<C-t>'] = false,
-			['<C-y>'] = 'actions.copy_entry_path'
+			['<C-y>'] = 'actions.copy_entry_path',
+			['<C-r>'] = function()
+				local Path = require('plenary.path')
+				local oil = require('oil')
+				local file = oil.get_cursor_entry().name
+				local dir = oil.get_current_dir()
+				local path = Path:new(dir .. file)
+				vim.ui.input(
+					{
+						prompt = "New File Name",
+						default = path:absolute(),
+						completion = 'file'
+					},
+					function(input)
+						if input ~= nil and input ~= "" then
+							path:rename { new_name = Path:new(input).filename }
+							require('oil.actions').refresh.callback()
+						end
+					end)
+			end
 		}
 	},
 	lazy = false,
