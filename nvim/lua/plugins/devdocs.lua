05f79491e03906c8b3bc7eb3f92d14a5a2b7174c
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-11-29 09:28:33 -0500

    changes

---
+++ nvim/lua/plugins/devdocs.lua
@@ -0,0 +1,21 @@
+return {
+	"luckasRanarison/nvim-devdocs",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	opts = {
+		wrap = true,
+		after_open = function(bufnr)
+			vim.api.nvim_buf_set_keymap(bufnr, 'n', 'q', ':close<CR>', {})
+		end
+	},
+	lazy = false,
+	keys = {
+		{ '<leader>hf', '<CMD>DevdocsOpenCurrentFloat<CR>', desc = "Dev Docs Open Current Float" },
+		{ '<leader>hd', '<CMD>DevdocsOpenCurrent<CR>',      desc = "Dev Docs Open Current" },
+		{ '<leader>ha', '<CMD>DevdocsOpen<CR>',             desc = "Dev Docs Open" },
+		{ '<leader>hs', '<CMD>DevdocsOpenFloat<CR>',        desc = "Dev Docs Open Float" }
+	}
+}
