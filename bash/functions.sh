1b31485c2e633406fe0ac438cab24bada4cb84b6
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-12-05 12:30:48 -0500

    changes to fzf and cli

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,69 +1,30 @@
 #!/bin/sh
 
-# Theory behind naming functions:
-# Especially for functions that use fzf to extend a function, it will be in the notation of "base cmd" + "operation"
-# For example, to open a git project with nvim, the command would be nvimg, similarly to cd to a git project woudl be cdg
-
-# shortcut for cd ..
-cdd() {
-	cd ..
-}
-
-cddev() {
-	cd ~/dev
-}
-
-fzf_git_project() {
-	local selection
-	selection=$(ls ~/git | fzf  --preview='ls -l ~/git/{}' --header-first --header="$1") &&
-		echo ~/git/$selection
-}
-
-cdg() {
-	local dir
-	dir=$(fzf_git_project "cd to git project") && cd "$dir"
-}
 
 cdv() {
-	local selection
-	selection=$(ls ~/dev | fzf --preview='project_info ~/dev/{}' --header-first --header="Open Development Project") && cd ~/dev && cd $selection
+	local development
+	development=$(ls $DEV | fzf --preview='ls -l $DEV/{}' --header-first --header="cd to dev project") && 
+		cd $DEV/$development
 }
 
 cddot() {
 	cd $DOT_FILES
 }
 
-fzf_directory() {
-	find . -type d | fzf --preview='ls -l {}' --header-first --header="$1"
-}
-
-cdf() {
-	local dir
-	dir=$(fzf_directory "cd to directory") && cd "$dir"
-}
-
-nvimf() {
-	local dir
-	dir=$(fzf_directory "open directory in neovim") && nvim $dir
-}
-
-fzf_file() {
-	find . -type f | fzf --preview='cat {}' --header-first --header="$1"
-}
-
-cdgr() {
-	cd $(git rev-parse --show-toplevel)
-}
-
-cdt() {
-	cd ~/tmp
+cddev() {
+	cd $DEV
 }
 
 cdgit() {
 	cd ~/git
 }
 
-nvd() {
-	local selection
-	selection=$(ls ~/dev | fzf --preview='project_info ~/dev/{}' --header-first --header="Open Development Project") && cd ~/dev && cd $selection && nvim .
+cdg() {
+	local development
+	development=$(ls ~/git | fzf --preview='ls -l ~/git/{}' --header-first --header="cd to git repo") && 
+		cd ~/git/$development
+}
+
+gb() {
+	git branch
 }
--- bash/fzf.sh
+++ bash/fzf.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 export FZF_DEFAULT_OPTS='--layout=reverse --info=inline --color=fg:#cad3f5,bg:-1,hl:#ee99a0 --color=fg+:#b7bdf8,bg+:,hl+:#ed8796 --color=info:#b7bdf8,prompt:#b7bdf8,pointer:#b7bdf8 --color=marker:#b7bdf8,spinner:#b7bdf8,header:#b7bdf8,border:#b7bdf8,bg+:#24273a'
+export FZF_CTRL_T_COMMAND='fd --type f --strip-cwd-prefix'
+export FZF_ALT_C_COMMAND='fd --type d --strip-cwd-prefix'
