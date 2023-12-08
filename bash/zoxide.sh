c39be99d6f0e298ae1274b196c872bdc66918fb7
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-12-08 09:40:58 -0500

    Added zoxide

--- bash/zoxide.sh
+++ bash/zoxide.sh
@@ -1,10 +1,5 @@
 # Alt + Z for zoxide fzf
 
-# export _ZO_FZF_OPTS="$FZF_DEFAULT_OPTS"
 export _ZO_FZF_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --bind=ctrl-z:ignore --reverse --scheme=path ${FZF_DEFAULT_OPTS-} ${FZF_ALT_C_OPTS-} +m"
 
-__zoxide_select__() {
-	dir=$(zi && pwd) && printf 'builtin cd -- %q' "$dir"
-}
-
-bind -m emacs-standard '"\ez": " \C-b\C-k \C-u`__zoxide_select__`\e\C-e\er\C-m\C-y\C-h\e \C-y\ey\C-x\C-x\C-d"'
+bind '"\ez": "zi\n"'
