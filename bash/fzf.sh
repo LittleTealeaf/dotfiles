d5347d2826af2a259e76e39a5a59ab384057bd55
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-12-10 12:12:52 -0500

    custom

--- bash/fzf.sh
+++ bash/fzf.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # Catppuccin Colors
-export FZF_DEFAULT_OPTS="--color=bg:#1e1e2e,fg:#cdd6f4 \
-	--color=border:#45475a,header:#cba6f7,pointer:#f5e0dc \
-	--color=marker:#f5e0dc,info:#cba6f7,prompt:#cba6f7 \
-	--color=fg+:#cdd6f4,bg+:#313244,hl:#f38ba8,hl+:#f38ba8 \
-	--color=spinner:#cba6f7"
+# export FZF_DEFAULT_OPTS="--color=bg:#1e1e2e,fg:#cdd6f4 \
+# 	--color=border:#45475a,header:#cba6f7,pointer:#f5e0dc \
+# 	--color=marker:#f5e0dc,info:#cba6f7,prompt:#cba6f7 \
+# 	--color=fg+:#cdd6f4,bg+:#313244,hl:#f38ba8,hl+:#f38ba8 \
+# 	--color=spinner:#cba6f7"
 
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --height=20 --border --layout=reverse --info=inline"
+# Catppuccin Colors that mimic the telescope appearance
+export FZF_DEFAULT_OPTS="--color=bg:#181826,fg:#cdd6f4,border:#181826 \
+	--color=header:#cdd6f4,pointer:#cdd6f4,info:#89b4fb \
+	--color=fg+:#cdd6f4,bg+:#45475b \
+	--color=hl:#89b4fb,hl+:#89b4fb"
+
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --height=20 --layout=reverse --info=inline"
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --bind 'tab:down' --bind 'btab:up' --bind 'ctrl-a:toggle-all' --bind 'alt-enter:toggle'"
 export FZF_CTRL_T_COMMAND="fd --type f --strip-cwd-prefix"
 export FZF_ALT_C_COMMAND='fd --type d --strip-cwd-prefix'
