9abe5facbd69a29c581870cfabff9a986114c487
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-03-21 11:12:19 -0400

    update noice config

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -7,58 +7,57 @@ return {
 			'MunifTanjim/nui.nvim'
 		},
 		lazy = false,
-		opts = {
-			cmdline = {
-				view = 'cmdline',
-				format = {
-					cmdline = {
-						icon = ">"
+		config = function()
+			local noice = require('noice')
+
+			noice.setup({
+				cmdline = {
+					view = 'cmdline',
+					format = {
+						cmdline = {
+							icon = ">"
+						},
+					}
+				},
+				lsp = {
+					override = {
+						['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+						['vim.lsp.util.stylize_markdown'] = true,
+						['cmp.entry.get_documentation'] = true,
 					},
-				}
-			},
-			lsp = {
-				override = {
-					['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-					['vim.lsp.util.stylize_markdown'] = true,
-					['cmp.entry.get_documentation'] = true,
+					messages = {
+						enabled = true,
+						view = 'mini'
+					},
+					hover = {
+						silent = true,
+					},
+				},
+				presets = {
+					lsp_doc_border = Transparent,
+					inc_rename = true
+				},
+				commands = {
+					history = {
+						view = "popup"
+					},
+					errors = {
+						view = "popup"
+					}
 				},
 				messages = {
-					enabled = true,
-					view = 'mini'
+					view_history = 'popup',
+					view_search = false,
 				},
-				hover = {
-					silent = true,
-				},
-			},
-			presets = {
-				lsp_doc_border = Transparent,
-				inc_rename = true
-			},
-			commands = {
-				history = {
-					view = "popup"
-				},
-				errors = {
-					view = "popup"
-				}
-			},
-			messages = {
-				view_history = 'popup',
-				view_search = false,
-			},
-		},
-		keys = {
-			{ '<leader>nl', '<cmd>Noice last<CR>',    desc = "Open Last Notification" },
-			{ '<leader>nm', '<cmd>messages<CR>',      desc = "Open Messages" },
-			{ '<leader>nc', '<cmd>Noice dismiss<CR>', desc = "Clear Messages" },
-			{ '<leader>nn', '<cmd>Noice<CR>',         desc = "Noice History" },
-			{ '<leader>ne', '<cmd>Noice errors<CR>',  desc = "Noice Errors" },
-			{
-				'<S-Enter>',
-				function() require('noice').redirect(vim.fn.getcmdline()) end,
-				desc = "Redirect Cmdline",
-				mode = 'c'
-			}
-		}
+			})
+
+			vim.keymap.set('n', '<leader>nl', '<cmd>Noice last<CR>', { desc = "Open Last Notification" })
+			vim.keymap.set('n', '<leader>nm', '<cmd>messages<CR>', { desc = "Open Messages" })
+			vim.keymap.set('n', '<leader>nc', '<cmd>Noice dismiss<CR>', { desc = "Clear Messages" })
+			vim.keymap.set('n', '<leader>nn', '<cmd>Noice dismiss<CR>', { desc = "Clear Messages" })
+			vim.keymap.set('n', '<leader>ne', '<cmd>Noice errors<CR>', { desc = "Noice Errors" })
+
+			vim.keymap.set('c', '<S-Enter>', function() noice.redirect(vim.fn.getcmdline()) end, { desc = "Redirect Command" })
+		end,
 	}
 }
