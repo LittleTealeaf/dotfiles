ba6dc1a1777ee860bdd6c2bed15b4258eda64f13
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-07-14 11:50:56 -0400

    added which key

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -1,6 +1,9 @@
 return {
 	{
 		'folke/which-key.nvim',
+		dependencies = {
+			{ 'echasnovski/mini.icons', version = false }
+		},
 		event = 'VeryLazy',
 		init = function()
 			vim.o.timeout = true
