b896e95d3d05f61da119887651ff82b78da3c945
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-12-20 10:31:26 -0500

    changes

--- nvim/lua/plugins/rustaceanvim.lua
+++ nvim/lua/plugins/rustaceanvim.lua
@@ -30,74 +30,75 @@ return {
 	ft = { 'rust' },
 	init = function()
 		vim.g.rustaceanvim =
-				function()
+		{
+			tools = {
+				test_executor_alias = "background",
+				executor = {
+					execute_command = execute_from_toggleterm
+				},
+				['hover-actions'] = {
+					replace_builtin_hover = true
+				},
+				hover_actions = {
+					border = Transparent and 'rounded' or {
+						{ '' },
+						{ '' },
+						{ '' },
+						{ '' },
+						{ '' },
+						{ '' },
+						{ '' },
+						{ '' },
+					},
+				}
+			},
+			dap = {
+				auto_generate_source_map = true
+			},
+			server = {
+				on_attach = function(client, bufnr)
+					vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+					vim.keymap.set('n', '<leader>clu', lspcmd { 'debuggables', bang = true },
+						{ buffer = bufnr, desc = "Rust Last Debuggable" })
+					vim.keymap.set('n', '<leader>cli', lspcmd { 'runnables', bang = true },
+						{ buffer = bufnr, desc = "Rust Last Runnable" })
+					vim.keymap.set('n', '<leader>clr', lspcmd { 'runnables' }, { buffer = bufnr, desc = "Rust Runnables" })
+					vim.keymap.set('n', '<leader>cla', lspcmd { 'codeAction' }, { buffer = bufnr, desc = "Code Actions" })
+					vim.keymap.set('n', '<leader>cld', lspcmd { 'debuggables' }, { buffer = bufnr, desc = "Rust Debuggabes" })
+					vim.keymap.set('n', '<leader>cle', lspcmd { 'renderDiagnostic' },
+						{ buffer = bufnr, desc = "Render Diagnostic" })
+					vim.keymap.set('n', '<leader>clp', lspcmd { 'parentModule' }, { buffer = bufnr, desc = "Parent Module" })
+					vim.keymap.set('n', '<leader>clc', lspcmd { 'flyCheck' }, { buffer = bufnr, desc = "Clippy Check" })
+					vim.keymap.set('n', '<leader>clt', lspcmd { 'openCargo' }, { buffer = bufnr, desc = "Open Cargo.toml" })
+					vim.keymap.set('v', 'K', lspcmd { 'hover', 'range' }, { buffer = bufnr, desc = "Hover Range" })
+					vim.keymap.set('n', '<leader>clk', lspcmd { 'moveItem', 'up' }, { buffer = bufnr, desc = "Move Item Up" })
+					vim.keymap.set('n', '<leader>clj', lspcmd { 'moveItem', 'down' },
+						{ buffer = bufnr, desc = "Move Item Down" })
+					vim.keymap.set('n', '<leader>clh', lspcmd { 'joinLines' }, { buffer = bufnr, desc = "Join Lines" })
+				end,
+				settings = function()
 					return {
-						tools = {
-							test_executor = "background",
-							executor = {
-								execute_command = execute_from_toggleterm
+						['rust-analyzer'] = {
+							completion = {
+								postfix = {
+									enable = false
+								}
 							},
-							hover_actions = {
-								border = Transparent and 'rounded' or {
-									{ '' },
-									{ '' },
-									{ '' },
-									{ '' },
-									{ '' },
-									{ '' },
-									{ '' },
-									{ '' },
-								},
-							}
-						},
-						dap = {
-							auto_generate_source_map = true
-						},
-						server = {
-							on_attach = function(client, bufnr)
-								vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-								vim.keymap.set('n', '<leader>clu', lspcmd { 'debuggables', bang = true },
-									{ buffer = bufnr, desc = "Rust Last Debuggable" })
-								vim.keymap.set('n', '<leader>cli', lspcmd { 'runnables', bang = true },
-									{ buffer = bufnr, desc = "Rust Last Runnable" })
-								vim.keymap.set('n', '<leader>clr', lspcmd { 'runnables' }, { buffer = bufnr, desc = "Rust Runnables" })
-								vim.keymap.set('n', '<leader>cla', lspcmd { 'codeAction' }, { buffer = bufnr, desc = "Code Actions" })
-								vim.keymap.set('n', '<leader>cld', lspcmd { 'debuggables' }, { buffer = bufnr, desc = "Rust Debuggabes" })
-								vim.keymap.set('n', '<leader>cle', lspcmd { 'renderDiagnostic' },
-									{ buffer = bufnr, desc = "Render Diagnostic" })
-								vim.keymap.set('n', '<leader>clp', lspcmd { 'parentModule' }, { buffer = bufnr, desc = "Parent Module" })
-								vim.keymap.set('n', '<leader>clc', lspcmd { 'flyCheck' }, { buffer = bufnr, desc = "Clippy Check" })
-								vim.keymap.set('n', '<leader>clt', lspcmd { 'openCargo' }, { buffer = bufnr, desc = "Open Cargo.toml" })
-								vim.keymap.set('v', 'K', lspcmd { 'hover', 'range' }, { buffer = bufnr, desc = "Hover Range" })
-								vim.keymap.set('n', '<leader>clk', lspcmd { 'moveItem', 'up' }, { buffer = bufnr, desc = "Move Item Up" })
-								vim.keymap.set('n', '<leader>clj', lspcmd { 'moveItem', 'down' },
-									{ buffer = bufnr, desc = "Move Item Down" })
-								vim.keymap.set('n', '<leader>clh', lspcmd { 'joinLines' }, { buffer = bufnr, desc = "Join Lines" })
-							end,
-							settings = function()
-								return {
-									['rust-analyzer'] = {
-										completion = {
-											postfix = {
-												enable = false
-											}
-										},
-										check = {
-											command = "clippy"
-										},
-										workspace = {
-											symbol = {
-												search = {
-													kind = 'only_types',
-													limit = 1024
-												}
-											}
-										}
+							check = {
+								command = "clippy"
+							},
+							workspace = {
+								symbol = {
+									search = {
+										kind = 'only_types',
+										limit = 1024
 									}
 								}
-							end
-						},
+							}
+						}
 					}
 				end
+			},
+		}
 	end
 }
