d18a939f039f47f98c3960fb6f598a054fdccf79
Thomas Kwashnak <thomaskwashnak@gmail.com>
2025-10-11 16:01:20 -0400

    changes

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,92 +1,77 @@
-local LSP_CONFIG = require('lsp_config')
+-- File: lua/plugins/lsp.lua
 
-local function on_lsp_attach(client, _)
+-- This table for your custom server settings remains the same.
+local LSP_CONFIG = {
+	rust_analyzer = {
+		settings = {
+			['rust-analyzer'] = {
+				check = {
+					command = "clippy",
+				},
+			},
+		},
+	},
+	-- Add other custom server configs here
+}
+
+-- Your on_attach function remains the same.
+local function on_lsp_attach(client, bufnr)
 	if client.server_capabilities.inlayHintProvider then
-		vim.lsp.inlay_hint.enable(true)
+		vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
 	end
-end
-
-local function check_system(system)
-	if system['bin'] ~= nil then
-		local file = io.open('/usr/bin/' .. system['bin'], 'r')
-		if file then
-			file:close()
-			return '/usr/bin/' .. system['bin']
-		else
-			return nil
-		end
-	end
-	return nil
+	-- Add your keymaps here, for example:
+	-- vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
 end
 
 return {
 	'williamboman/mason-lspconfig.nvim',
 	dependencies = {
-		{ 'hrsh7th/cmp-nvim-lsp' },
-		{ 'williamboman/mason.nvim', },
-		{ 'neovim/nvim-lspconfig', },
+		'hrsh7th/cmp-nvim-lsp',
+		'williamboman/mason.nvim',
+		'neovim/nvim-lspconfig',
 	},
 	event = 'VeryLazy',
 	cond = vim.g.features.lsp,
 	config = function()
-		local lspconfig = require('lspconfig')
+		-- Define the list of servers you want Mason to install.
+		local servers = {
+			'lua_ls',
+			'rust_analyzer',
+			'pyright',
+			-- Add all other servers you want installed here
+		}
+
+		-- Setup mason-lspconfig to install the servers from the list above.
+		require('mason-lspconfig').setup({
+			ensure_installed = servers,
+			-- automatic_enable is true by default, so we don't need to set it.
+			-- This is what handles calling vim.lsp.enable() for you.
+		})
+
+		-- Get default capabilities from cmp-nvim-lsp.
 		local capabilities = require('cmp_nvim_lsp').default_capabilities()
 		capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-		require('mason-lspconfig').setup({
-			function(server_name)
-				local lsp_config = LSP_CONFIG[server_name]
-				local settings = nil
-				local before_init = nil
-				if lsp_config ~= nil then
-					settings = lsp_config.settings
-					if lsp_config.before_init ~= nil then
-						before_init = lsp_config.before_init()
+		-- This loop is still necessary to apply your custom settings to each server.
+		for _, server_name in ipairs(servers) do
+			local user_config = LSP_CONFIG[server_name] or {}
+
+			vim.lsp.config(server_name, {
+				capabilities = capabilities,
+				on_attach = function(client, bufnr)
+					-- Call your shared on_attach function
+					on_lsp_attach(client, bufnr)
+					-- Call any server-specific on_attach functions
+					if user_config.on_attach then
+						user_config.on_attach(client, bufnr)
 					end
-				end
-				lspconfig[server_name].setup {
-					capabilities = capabilities,
-					before_init = before_init,
-					on_attach = function(client, bufnr)
-						on_lsp_attach(client, bufnr)
-						if lsp_config ~= nil and lsp_config.on_attach ~= nil then
-							lsp_config.on_attach(client, bufnr)
-						end
-					end,
-					settings = settings
-				}
-			end,
-			jdtls = function() end
-		})
+				end,
+				settings = user_config.settings,
+				before_init = user_config.before_init,
+			})
 
-
-
-		for lsp, config in pairs(LSP_CONFIG) do
-			if config['system'] ~= nil then
-				local path = check_system(config['system'])
-				if path ~= nil then
-					local settings = nil
-					local before_init = nil
-					if config ~= nil then
-						settings = config.settings
-						if config.before_init ~= nil then
-							before_init = config.before_init()
-						end
-					end
-
-					lspconfig[lsp].setup {
-						capabilities = capabilities,
-						before_init = before_init,
-						settings = settings,
-						on_attach = function(client, bufnr)
-							on_lsp_attach(client, bufnr)
-							if config ~= nil and config.on_attach ~= nil then
-								config.on_attach(client, bufnr)
-							end
-						end
-					}
-				end
-			end
+			-- The vim.lsp.enable(server_name) call is NO LONGER NEEDED here!
+			-- mason-lspconfig handles it automatically.
 		end
 	end
 }
