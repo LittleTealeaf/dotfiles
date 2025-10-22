2fdba78acf7b116c96029466cb2a666c001faacf
Thomas Kwashnak <thomaskwashnak@gmail.com>
2025-10-22 09:31:24 -0400

    add obsidian configuration

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,7 +1,33 @@
+local function get_workspaces(base_path)
+	if not base_path or base_path == "" then
+		return {}
+	end
+
+	local directories = {}
+
+	local command = string.format("find %s -type d", base_path)
+
+	local handle = io.popen(command)
+	if not handle then
+		print("Error: Could not execute find command.")
+		return {}
+	end
+
+	for path in handle:lines() do
+		local name = string.match(path, "([^/]+)/*$")
+		table.insert(directories, {
+			name = name,
+			path = path
+		})
+	end
+
+	handle:close()
+	return directories
+end
+
 return {
 	"epwalsh/obsidian.nvim",
-	cond = true,
-	enabled = true,
+	enabled = vim.g.features.obsidian,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		'hrsh7th/nvim-cmp',
@@ -15,11 +41,6 @@ return {
 				end
 			},
 		},
-		workspaces = {
-			{
-				name='aethelspark',
-				path='~/Documents/aethelspark'
-			}
-		}
+		workspaces = get_workspaces(OBSIDIAN_ENV)
 	}
 }
