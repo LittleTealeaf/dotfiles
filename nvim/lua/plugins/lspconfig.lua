0503b3493ef8c546b8ec3d5642e2597cd4081b34
Thomas Kwashnak <thomaskwashnak@gmail.com>
2025-10-24 08:55:50 -0400

    changes

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,14 +1,30 @@
+local LSP_SERVERS = {
+	"ts_ls",
+	"lua_ls",
+	"codelldb",
+	"clangd"
+}
+
+
 return {
 	'mason-org/mason-lspconfig.nvim',
 	dependencies = {
 		{ 'mason-org/mason.nvim', opts = {} },
 		'neovim/nvim-lspconfig'
 	},
-	opts = {
-		automatic_enable = {
-			exclude = {
-				'rust_analyzer'
+	cond = vim.g.features.lsp,
+	config = function()
+		for _, lsp in ipairs(LSP_SERVERS) do
+			vim.lsp.enable(lsp)
+		end
+
+
+		require('mason-lspconfig').setup({
+			automatic_enable = {
+				exclude = {
+					'rust_analyzer'
+				}
 			}
-		}
-	}
+		})
+	end,
 }
--- nvim/lua/plugins/mason.lua
+++
@@ -1,8 +0,0 @@
--- TODO: Go through package list manually and instead create a script to check / use the system installed version
-return {
-	{
-		'williamboman/mason.nvim',
-		opts = {},
-		cond = vim.g.features.lsp,
-	},
-}
