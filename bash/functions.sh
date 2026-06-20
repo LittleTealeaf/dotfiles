4e8d629b5aedc09abe85b4c1eb4ae467ab1c1db4
Thomas Kwashnak <thomaskwashnak@gmail.com>
2026-06-20 09:50:25 -0400

    update commands

--- bash/functions.sh
+++ bash/functions.sh
@@ -24,22 +24,27 @@ cdg() {
 		cd ~/git/$development
 }
 
-ga() {
-	git forgit add
-}
+# ga() {
+# 	git forgit add
+# }
 
 cdt() {
 	cd ~/tmp
 }
 
-fdf() {
-	fd | fzf
-}
+if command -v fd &> /dev/null && command -v fzf &> /dev/null; then
+	fdf() {
+		fd | fzf
+	}
+fi
 
-function y() {
-	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-	command yazi "$@" --cwd-file="$tmp"
-	IFS= read -r -d '' cwd < "$tmp"
-	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
-	command rm -f -- "$tmp"
-}
+
+if command -v yazi &> /dev/null; then
+	function y() {
+		local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+		command yazi "$@" --cwd-file="$tmp"
+		IFS= read -r -d '' cwd < "$tmp"
+		[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+		command rm -f -- "$tmp"
+	}
+fi
