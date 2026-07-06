01f5993892604b05ca75bb88d5a95798154906a8
Thomas Kwashnak <thomaskwashnak@gmail.com>
2026-07-05 21:35:17 -0400

    update trouble

--- nvim/lua/ui/trouble.lua
+++ nvim/lua/ui/trouble.lua
@@ -7,30 +7,42 @@ vim.api.nvim_create_autocmd('FileType', {
 	pattern = 'qf',
 	group = vim.api.nvim_create_augroup('tea.quickfix_to_trouble', {}),
 	callback = function()
+		local winid = vim.api.nvim_get_current_win()
+		local wininfo = vim.fn.getwininfo(winid)[1]
 		vim.schedule(function()
-			vim.cmd('cclose')
-			trouble.open('quickfix')
+			if wininfo['quickfix'] == 1 then
+				vim.cmd('cclose')
+				trouble.open('quickfix')
+			end
+			if wininfo['loclist'] == 1 then
+				vim.cmd('lclose')
+				trouble.open('loclist')
+			end
 		end)
 	end
 })
 
-
-trouble.setup({
-	focus = true,
-	win = {
-		size = {
-			height = 12,
-			width = 50
-		},
-		wo = {
-			cursorline = true,
-			winhighlight = "CursorLine:TroubleCursorLine",
-		},
-	},
-	keys = {
-		["<M-CR>"] = "jump_close"
-	},
-	warn_no_results = false,
+vim.api.nvim_create_autocmd('VimEnter', {
+	group = vim.api.nvim_create_augroup('tea.trouble.setup', {}),
+	callback = function()
+		trouble.setup({
+			focus = true,
+			win = {
+				size = {
+					height = 12,
+					width = 50
+				},
+				wo = {
+					cursorline = true,
+					winhighlight = "CursorLine:TroubleCursorLine",
+				},
+			},
+			keys = {
+				["<M-CR>"] = "jump_close"
+			},
+			warn_no_results = false,
+		})
+	end
 })
 
 
@@ -41,3 +53,4 @@ vim.keymap.set("n", "<leader>tq", function() trouble.open('quickfix') end, { des
 vim.keymap.set("n", "<leader>th", trouble.close, { desc = "Close" })
 vim.keymap.set('n', '<leader>tf', function() trouble.open('snacks_files') end, { desc = "Finder" })
 vim.keymap.set('n', '<leader>td', function() trouble.open('snacks') end, { desc = "Snacks" })
+vim.keymap.set('n', '<leader>tl', function() trouble.open('loclist') end, { desc = "Loclist" })
