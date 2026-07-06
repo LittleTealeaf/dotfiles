1939694628eee7300e9e0d43ef4fb1b79c9210e1
Thomas Kwashnak <thomaskwashnak@gmail.com>
2026-07-05 21:15:55 -0400

    oil preview on default

--- nvim/lua/ui/oil.lua
+++ nvim/lua/ui/oil.lua
@@ -1,6 +1,5 @@
 vim.pack.add({ Github('stevearc/oil.nvim') })
 local oil = require('oil')
-local PREFER_FLOAT = true
 
 oil.setup({
 	view_options = {
@@ -11,6 +10,7 @@ oil.setup({
 	},
 	float = {
 		border = "rounded",
+		preview_split = 'right',
 		padding = 3,
 	},
 	keymaps = {
@@ -21,32 +21,26 @@ oil.setup({
 		['<C-t>'] = 'actions.open_cmdline',
 		['<C-y>'] = 'actions.copy_entry_path',
 		['<C-l>'] = 'actions.send_to_loclist',
-		['<C-r>'] = 'actions.refresh'
+		['<C-r>'] = 'actions.refresh',
+		['<C-u>'] = 'actions.preview_scroll_up',
+		['<C-d>'] = 'actions.preview_scroll_down'
 	}
 })
--- Open Workspace in Float
-vim.keymap.set("n", PREFER_FLOAT and "<leader>ew" or "<leader>eq", function()
-	local cwd = vim.uv.cwd()
-	if oil.get_current_dir() ~= nil then
-		oil.open(cwd)
-	else
-		oil.open_float(cwd)
-	end
-end, { desc = "Open Workspace in Float" })
 
--- Open Workspace
-vim.keymap.set("n", PREFER_FLOAT and "<leader>eq" or "<leader>ew", function()
-	oil.open(vim.uv.cwd())
-end, { desc = "Open Workspace" })
 
--- Open Parent Directory in Float
-vim.keymap.set("n", PREFER_FLOAT and "<leader>er" or "<leader>et", function()
-	if oil.get_current_dir() ~= nil then
-		oil.open()
-	else
-		oil.open_float()
-	end
-end, { desc = "Open Parent Directory in Float" })
-
--- Open Parent Directory
-vim.keymap.set("n", PREFER_FLOAT and "<leader>et" or "<leader>er", oil.open, { desc = "Open Parent Directory" })
+vim.keymap.set(
+	'n',
+	'<leader>ew',
+	function()
+		oil.open_float(vim.uv.cwd(), { preview = { horizontal = true } })
+	end,
+	{ desc = "Open Workspace in Oil" }
+)
+vim.keymap.set(
+	'n',
+	'<leader>er',
+	function()
+		oil.open_float(nil, { preview = { horizontal = true } })
+	end,
+	{ desc = 'Open File Location in Oil' }
+)
